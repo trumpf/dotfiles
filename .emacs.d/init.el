bd77a510e91618aa9aba4038e55481ea32157df9
Alkaloid <4aassembly@gmail.com>
2013-10-18 10:00:40 +0900

    Configured the minimum settings for Auto Install and Anything.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -21,6 +21,13 @@
 
 (add-to-list 'load-path "~/.emacs.d/elisp")
 
+;; For Auto Install
+(when (require 'auto-install nil t)
+  (setq auto-install-directory "~/.emacs.d/elisp/")
+  (auto-install-update-emacswiki-package-name t)
+  ;; (setq url-proxy-services '(("http" . "localhost:8339")))
+  (auto-install-compatibility-setup))
+
 ;; Python mode
 (require 'python-mode)
 (autoload 'python-mode "python-mode" "Python Mode." t)
@@ -32,6 +39,38 @@
 ;; To save buffers which have shebang with enabling executable bits
 (add-hook 'after-save-hook
           'executable-make-buffer-file-executable-if-script-p)
+
+;; Anything
+(when (require 'anything nil t)
+  (setq
+   anything-idle-delay 0.3
+   anything-input-idle-delay 0.2
+   anything-candidate-number-limit 100
+   anything-quick-update t
+   anything-enable-shortcuts 'alphabet)
+
+  (when (require 'anything-config nil t)
+    (setq anything-su-or-sudo "sudo"))
+
+  (require 'anything-match-plugin nil t)
+
+  (when (and (executable-find "cmigemo")
+             (require 'migemo nil t))
+    (require 'anything-migemo nil t))
+
+  (when (require 'aything-complete nil t)
+    (anything-lisp-complete-symbol-set-timer 150))
+
+  (require 'anything-show-completion nil t)
+
+  (when (require 'auto-install nil t)
+    (require 'anything-auto-install nil t))
+
+  (when (require 'descbinds-anything nil t)
+    (descbinds-anything-install))
+
+  (add-to-list 'anything-sources 'anything-c-source-emacs-commands)
+  (define-key global-map (kbd "C-l") 'anything))
 
 ;; Misc
 (setq inhibit-startup-message t)
